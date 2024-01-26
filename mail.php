6ec73ecc18e6928413431ba47ecea6358ff6db70
vbb08 <67701977+vbb08@users.noreply.github.com>
2024-01-26 23:08:27 +0100

    Create mail.php

---
+++ mail.php
@@ -0,0 +1,53 @@
+<?php
+ 
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+ 
+//required files
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
+ 
+//Create an instance; passing `true` enables exceptions
+if (isset($_POST["send"])) {
+ 
+  $mail = new PHPMailer(true);
+ 
+    //Server settings
+    $mail->isSMTP();                              //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';       //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;             //Enable SMTP authentication
+    $mail->Username   = '###############@gmail.com';   //SMTP write your email
+    $mail->Password   = '#############';      //SMTP password
+    $mail->SMTPSecure = 'ssl';            //Enable implicit SSL encryption
+    $mail->Port       = 465;                                    
+ 
+    //Recipients
+    $mail->setFrom( $_POST["email"], $_POST["name"]); // Sender Email and name
+    $mail->addAddress('###############@gmail.com');     //Add a recipient email  
+    $mail->addReplyTo($_POST["email"], $_POST["name"]); // reply to sender email
+ 
+    //Content
+    $mail->isHTML(true);               //Set email format to HTML
+    $mail->Subject = $_POST["subject"];   // email subject headings
+    $mail->Body = "Email: {$_POST["email"]}\n\nName: {$_POST["name"]}\n\nMessage: {$_POST["message"]}";   //email body (email/name/message)
+    
+      
+    // Validation and Success/Failure sent message alert
+    $mail->send();
+    if ($mail->send()) {
+      echo 
+      "
+      <script> 
+     alert('Message was sent successfully!');
+     document.location.href = 'index.html#contact';
+    </script>
+    "
+      ;
+    } else {
+      echo 'Email could not be sent. Error: ' . $mail->ErrorInfo;
+    }
+
+}
